1f4f685572fe8e3f3c4bedeb09b2723df653838c
dongdong69 <1204727226@qq.com>
2019-05-24 09:53:38 +0800

    floder day-5 added

--- week-03/day-03/main.py
+++ week-03/day-03/main.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, make_response
 from bs4 import BeautifulSoup
-import csv
+import csv, json
 
 # url = request.urlopen("http://interthing.org/dmls/species.html")
 # html = url.read()
@@ -22,8 +22,6 @@ lines = csv.reader(csvr)
 
 index, name, price, qty = 0, 1, 2, 3
 
-print(lines)
-
 for line in lines:
     line = line[0].split(';')
     information[line[name]] = {}
@@ -33,11 +31,123 @@ for line in lines:
 csvr.close()
 
 del information["name"]
-print(information)
 
 @app.route('/')
 def hello_world():
     return render_template('homePage.html')
+
+@app.route('/api/movies', methods=["POST", "GET", "PUT", "DELETE"])
+def apiMovie():
+
+    if request.method == 'POST':
+        print("POST!!!!!!!!!!!!!!!!")
+        headers = request.headers
+        key = headers.get("X-Api-Key")
+        print(key)
+
+        if key == "dongdong":
+            with open('dongdong69\week-03\day-03\\templates\movies.json', mode = 'r') as jsonr:
+                information_movies = json.loads(jsonr.read())
+
+            id_number = len(information_movies) + 1
+
+            response = request.get_data()
+            j_data =  json.loads(response)
+            j_data["id"] = id_number
+            information_movies.append(j_data)
+
+            with open('dongdong69\week-03\day-03\\templates\movies.json', mode = 'w') as jsonw:
+                json.dump(information_movies, jsonw)
+
+            return jsonify(j_data)
+        else:
+            return jsonify({"error": "EInvalid API_KEY"}), 403
+
+
+    if request.method == 'GET':
+        with open('dongdong69\week-03\day-03\\templates\movies.json', mode = 'r') as jsonr:
+            response = json.loads(jsonr.read())
+
+        #response = make_response("not found", 404)
+
+        response = jsonify(response)
+        return response
+
+    if request.method == 'PUT':
+
+        headers = request.headers
+        key = headers.get("X-Api-Key")
+
+        if key == "dongdong":
+            with open('dongdong69\week-03\day-03\\templates\movies.json', mode = 'r') as jsonr:
+                information_movies = json.loads(jsonr.read())
+                response = request.get_data()
+                j_data =  json.loads(response)
+                movie_id = j_data["id"]
+                found = False
+                for movie in information_movies:
+                    if movie['id'] == movie_id:
+                        found = True
+                        movie['title'] = j_data['title']
+                        movie['year'] = j_data['year']
+                        movie['genre'] = j_data['genre']
+                        movie['description'] = j_data['description']
+            
+            if not found:
+                return jsonify({"error": f"No movie found with {movie_id} ID"}), 404
+
+            else:
+                with open('dongdong69\week-03\day-03\\templates\movies.json', mode = 'w') as jsonw:
+                    json.dump(information_movies, jsonw)
+
+                return jsonify({"success": f"You have already changed the movie {movie_id}"})
+
+        else:
+            return jsonify({"error": "EInvalid API_KEY"}), 403
+
+
+    if request.method == 'DELETE':
+        headers = request.headers
+        key = headers.get("X-Api-Key")
+        if key == "dongdong":
+            with open('dongdong69\week-03\day-03\\templates\movies.json', mode = 'r') as jsonr:
+                information_movies = json.loads(jsonr.read())
+                response = request.get_data()
+                j_data =  json.loads(response)
+                movie_id = j_data["id"]
+                found = False
+                for movie in information_movies:
+                    if movie['id'] == movie_id:
+                        found = True
+                        break
+            if found:
+                information_movies.remove(movie)
+                with open('dongdong69\week-03\day-03\\templates\movies.json', mode = 'w') as jsonw:
+                    json.dump(information_movies, jsonw)
+
+                return jsonify({"success": f"You have already deleted the movie {movie_id}"})
+
+            else:
+                return jsonify({"error": f"No movie found with {movie_id} ID"}), 404
+
+
+        else:
+            return jsonify({"error": "EInvalid API_KEY"}), 403
+            
+
+
+
+@app.route('/api/movies/<movie_id>')
+def apiMovieId(movie_id):
+    #open and read the json file
+
+    with open('dongdong69\week-03\day-03\\templates\movies.json', mode = 'r') as jsonr:
+        response = json.loads(jsonr.read())[int(movie_id) - 1]
+
+    #response = make_response("not found", 404)
+
+    response = jsonify(response)
+    return response
 
 @app.route('/index')
 def hello_name():
@@ -104,6 +214,28 @@ def handPost():
     username = request.form.get('username1')
     password = request.form.get('password1')
     return "<h1>username:"+username+", password:"+password+"</h1>"
+
+@app.route('/movies')
+def movie():
+    with open('dongdong69\week-03\day-03\\templates\movies.json', mode = 'r') as jsonr:
+        information_movies = json.loads(jsonr.read())
+
+    title_list = []
+    for movie in information_movies:
+        title_list.append(movie["title"])
+    
+    return render_template('movie_title.html', title_list = title_list)
+
+@app.route('/edit-movie/<movie_id>', methods = ["GET"])
+def edit_movie(movie_id):
+    movie_id = int(movie_id)
+    with open('dongdong69\week-03\day-03\\templates\movies.json', mode = 'r') as jsonr:
+        information_movies = json.loads(jsonr.read())
+
+    movie = information_movies[movie_id - 1]
+    print(movie)
+    return render_template('movie_edit.html', movie = movie)
+    
 
 if __name__ == '__main__':
     app.run()
