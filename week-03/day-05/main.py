21a557d9976f392fe4b6b84a75cfef11f49c8cc8
dongdong69 <1204727226@qq.com>
2019-05-24 10:02:08 +0800

    random lang added

--- week-03/day-05/main.py
+++ week-03/day-05/main.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    language_John = ["Hello, John!", "你好约翰！", "سلام جان!", "こんにちはジョンさん！"]
+
+    random_index = random.randint(0, len(language_John) - 1)
+    return render_template('Random language.html', language = language_John[random_index])
+
+if __name__ == "__main__":
+    app.run()
